83e37a71dedfa0b1a731c134ac03368ef81cde7d
AbdaalZulfi <114151339+AbdaalZulfi@users.noreply.github.com>
2022-11-03 13:35:33 +0300

    Working Code

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
+import { obdinfo } from '../obdinfo';
+import * as _ from 'underscore';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-tab1',
@@ -7,10 +11,166 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
   private selectedSegment: string ='Sensor';
-  constructor() {}
+  lastConnectedToOBD;
+  receivedData;
+  constructor(private bluetooth:BluetoothSerial) {
+    this.lastConnectedToOBD = Date.now();
+  }
 
   segmentChanged(event : any){
   console.log(event.target.value);
   this.selectedSegment=event.target.value;
   }
+
+  searchOBD()
+  {
+    console.log('[INFO] Device Connec ted: '+JSON.stringify(this.bluetooth.isConnected()));
+
+
+    this.bluetooth.isConnected().then((success)=>{
+      this.bluetooth.write('P0103\r').then((tru)=>{
+        console.log('Hello:  ' + JSON.stringify(tru));
+
+      }).catch(er=>{console.log(er)})
+
+  
+    }
+
+    ).catch(err=>{      console.log('[INFO] Device Not Connected ');
+  })
+  }
+
+  DataReceived(data)
+  {
+    this.lastConnectedToOBD = Date.now();
+    var currentString, arrayOfCommands;
+    currentString = this.receivedData + data.toString('utf8'); // making sure it's a utf8 string
+    arrayOfCommands = currentString.split('>');
+    var forString;
+    if (arrayOfCommands.length < 2)
+     {
+      this.receivedData = arrayOfCommands[0];
+     } 
+    else {
+      for (var i = 0; i < arrayOfCommands.length; i++) {
+          forString = arrayOfCommands[i];
+          if (forString === '')
+             continue;
+          var multipleMessages = forString.split('\r');
+          for (var k = 0; k < multipleMessages.length; k++) {
+              var messageString = multipleMessages[k];
+              if (messageString === '') 
+                  continue;
+              var reply;
+              reply = this.parseOBDCommand(messageString);
+                this.btEventEmit('dataReceived', reply);
+                this.receivedData = '';
+            }
+        }
+    }
+  }
+  parseOBDCommand = function (hexString) {
+    var reply,
+        byteNumber,
+        valueArray; //New object
+  
+    reply = {};
+    if (hexString === "NO DATA" || hexString === "OK" || hexString === "?" || hexString === "UNABLE TO CONNECT" || hexString === "SEARCHING...") {
+        //No data or OK is the response, return directly.
+        reply.value = hexString;
+        return reply;
+    }
+  
+    hexString = hexString.replace(/ /g, ''); //Whitespace trimming //Probably not needed anymore?
+    valueArray = [];
+  
+    for (byteNumber = 0; byteNumber < hexString.length; byteNumber += 2) {
+        valueArray.push(hexString.substr(byteNumber, 2));
+    }
+  
+    if (valueArray[0] === "41") {
+        reply.mode = valueArray[0];
+        reply.pid = valueArray[1];
+        for (var i = 0; i < obdinfo.PIDS.length; i++) {
+            if (obdinfo.PIDS[i].pid == reply.pid) {
+                var numberOfBytes = obdinfo.PIDS[i].bytes;
+                reply.name = obdinfo.PIDS[i].name;
+                switch (numberOfBytes) {
+                    case 1:
+                        reply.value = obdinfo.PIDS[i].convertToUseful(valueArray[2]);
+                        break;
+                    case 2:
+                        reply.value = obdinfo.PIDS[i].convertToUseful2(valueArray[2], valueArray[3]);
+                        break;
+                    case 4:
+                        reply.value = obdinfo.PIDS[i].convertToUseful4(valueArray[2], valueArray[3], valueArray[4], valueArray[5]);
+                        break;
+                    case 6:
+                        reply.value = obdinfo.PIDS[i].convertToUseful6(valueArray[2], valueArray[3], valueArray[4], valueArray[5], valueArray[6], valueArray[7] );
+                        break;
+                }
+                break; //Value is converted, break out the for loop.
+            } 
+        }
+    } else if (valueArray[0] === "43") {
+        reply.mode = valueArray[0];
+        for (var ij = 0; ij < obdinfo.PIDS.length; ij++) {
+            if (obdinfo.PIDS[ij].mode == "03") {
+                reply.name = obdinfo.PIDS[ij].name;
+                reply.value = obdinfo.PIDS[ij].convertToUseful6(valueArray[1], valueArray[2], valueArray[3], valueArray[4], valueArray[5], valueArray[6]);
+            }
+        }
+    }
+    return reply;
+  };
+  
+  btEventEmit = function (event,text) {
+    var pdata={ts:0,name:"",value:"",};
+
+    if ( event!=='dataReceived' || text.value === 'NO DATA' || text.name === undefined || text.value === undefined) {
+        return;
+    }
+    console.log('[INFO] Metric for ' + text.name);
+    pdata = {ts:Date.now(),name:text.name,value:text.value};
+    this.liveStatsNumRecordsSinceConnected++;
+    this.execSql('INSERT INTO livemetricstable VALUES (?,?,?,?,?)', [null,pdata.ts, pdata.name, pdata.value, 0],'');
+    if (pdata.name=='rpm') { 
+      this.lastRPMmetricTimestamp = pdata.ts;
+      this.lastRPMmetricvalue = pdata.value;
+    }
+    if (pdata.name!=='location'){ 
+        if (this.liveMetrics[pdata.name]==undefined) {
+          var mt =_.findWhere(this.obdmetrics, { name: pdata.name }); 
+          this.liveMetrics[pdata.name]={};
+          this.liveMetrics[pdata.name].description = mt.description;
+          this.liveMetrics[pdata.name].name=mt.name;
+          this.liveMetrics[pdata.name].unit=mt.unit;
+          this.liveMetrics[pdata.name].type='';  
+        }
+        this.liveMetrics[pdata.name].value=pdata.value;
+        if (this.liveMetrics[pdata.name].unit=='sec.' || this.liveMetrics[pdata.name].type=='s' ) {
+          this.liveMetrics[pdata.name].value=moment.utc(parseInt(pdata.value)).format('HH:mm:ss'); 
+          this.liveMetrics[pdata.name].unit='';  
+          this.liveMetrics[pdata.name].type='s';  
+        }
+  } else {  // location data
+    if (this.liveMetrics['latitude']==undefined) {
+       this.liveMetrics['latitude']={};
+      this.liveMetrics['latitude'].description = 'Location: latitude';
+      this.liveMetrics['latitude'].name='';
+      this.liveMetrics['latitude'].unit='°'
+      this.liveMetrics['latitude'].type='';          
+    }
+    this.liveMetrics['latitude'].value=  JSON.parse(pdata.value).latitude;
+    if (this.liveMetrics['longitude']==undefined) {
+      this.liveMetrics['longitude']={};
+     this.liveMetrics['longitude'].description = 'Location: longitude';
+     this.liveMetrics['longitude'].name='';
+     this.liveMetrics['longitude'].unit='°'
+     this.liveMetrics['longitude'].type='';          
+    }
+    this.liveMetrics['longitude'].value=  JSON.parse(pdata.value).longitude;
+  }
+  }
+
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,8 +6,10 @@ import { ActionSheetController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataSrvService } from '../data-srv.service';
 import { IonModal } from '@ionic/angular';
-
-
+import { obdinfo } from '../obdinfo';
+import * as moment from 'moment';
+import * as _ from 'underscore';
+import { BLE } from '@awesome-cordova-plugins/ble/ngx';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -15,27 +17,37 @@ import { IonModal } from '@ionic/angular';
 })
 export class Tab2Page {
   @ViewChild(IonModal) modal: IonModal;
-
   Devices:paired[];
-  Devices$:Observable<paired>;
+  queue=['ATZ\r','ATSP0\r','0100\r'];
+  receivedData:string ='';
+  writeDelay: number=50;
   blue=true;
-
+  i=0;
+  btConnected=false;
+  btIntervalWriter: any;
+  pollerInterval;
+  obdcommands=[];
+  CarInfo=['03'];
+  SavedCarCommand=[];
+  RepeatTimer=0;
+  isModalOpen=false;
   constructor(private bluetooth:BluetoothSerial,
               private DataSrv:DataSrvService,
               private action:ActionSheetController,
               private permission:AndroidPermissions, 
               private alert: AlertController,
               private toastctrl:ToastController) {
-    this.Pair();
   }
+ 
+Pair()
+{
+  this.isModalOpen=true;
+  this.bluetooth.isEnabled().then(
 
-  Pair()
-  {
-    this.bluetooth.isEnabled().then(
-      res=>{this.listDevices();},
-      eror=>{this.bluetooth.enable();
-             this.listDevices();})
-  }
+    res=>{this.listDevices();},
+    eror=>{this.bluetooth.enable();
+            this.listDevices();})
+}
     /*
     this.checking=true;
    this.permission.checkPermission(this.permission.PERMISSION.BLUETOOTH_SCAN).then(
@@ -52,84 +64,13 @@ export class Tab2Page {
     
     */
    
-  listDevices() 
-  { 
-   this.bluetooth.list().then(
-    success => {this.Devices = success;}, 
-    error => {this.showError(error);}); 
-   this.bluetooth.setDeviceDiscoveredListener().subscribe(
-    device=>{
-      let flag=true;
-      this.Devices.forEach(element => {
-        if(element.id==device.id)
-        { flag=false;  }});
-     
-        if(flag)
-        {
-          this.Devices.push({address:device.address,class:device.class,id:device.id,name:device.name});
-      
-      }
-      else{flag=true;} 
-      
-    },
-    error=>
-      {alert('Error scan: ' + JSON.stringify(error));});
-      this.bluetooth.discoverUnpaired();  
-      console.log("After: "+JSON.stringify(this.Devices));
-      //alert('Devices: ' + JSON.stringify(this.Devices));
-  }
- 
-/*
-  listunPairedDevices() { 
-    
-    this.bluetooth.setDeviceDiscoveredListener().subscribe(device=>
-      {
-        alert("Found Device " +JSON.stringify(device));
-        this.AddDevice(device);
-        
-      },
-      error=>
-      {alert('Error scan: ' + JSON.stringify(error));});  
-      this.bluetooth.discoverUnpaired();
-      //this.Modal();
-    
-        
-        success.forEach(element => {
-        console.log(element);
-        this.pairedDevices.push(element);
-         if(Array.isArray(this.pairedDevices)&&this.pairedDevices.length)
-        {
-          this.pairedDevices.push(element);
-        }else{
-          let statement=true;
-          this.pairedDevices.forEach(variable => {
-            if(element.address==variable.address)
-            {
-              statement=false;
-            }
-            
-          });
+listDevices() 
+{ 
+  this.bluetooth.list().then(
+  success => {this.Devices = success;}, 
+  error => {this.showError(error);}); 
 
-          if(statement)
-          {
-            this.pairedDevices.push(element);
-          }else{
-            statement=true;
-          }
-          }
-           });
-       
-      //this.bluetooth.setDeviceDiscoveredListener()
-      //this.pairedDevices = success;
-     
-      
-  
-    //
-    //this.checking=false;
-    
-  }
-
- */
+}
   async showError(error) {
     let  alert = await this.alert.create({
     message: error ,
@@ -138,35 +79,212 @@ export class Tab2Page {
   });
   await alert.present(); 
 }
-
 connect(dvc)
 {
   if(dvc.address=="")
-  {
     this.showError("No Address");
-    
-
-  }else{
+  else{
     this.bluetooth.connect(dvc.address).subscribe(success=>
       {
         this.modal.dismiss(null, 'cancel');
         this.presentToast("Connected Successfully");
         this.blue=false;
+        this.btConnected = true;
+        this.deviceConnected(); 
 
       
     },error=>{
-        console.log(error);
+        alert("Connect Error: "+error);
+        this.btConnected = false;
       })
 
   }
+  
     
 
  
 }
+ deviceConnected()
+{
+  this.bluetooth.subscribe('>').subscribe
+  (success=>{
+   this.dataReceived(success)
+    
 
-disconnect()
+  }, error => {
+    alert('Device Connected, Subscribe error: ' + error);
+  });
+ 
+  this.InitiateOBD();
+  //this.RequstData();
+
+}
+dataReceived(data)
+{
+  
+
+  var Ans;
+  Ans={};
+  //alert("Data Received:- "+JSON.stringify(data));
+  var cmd, totalcmds, SingleString,multiVal;
+  cmd=this.receivedData+data.toString('utf8');
+  //console.log("CMD:  "+cmd);
+  totalcmds=cmd.split('>');
+  //console.log("TotalCMD:  "+totalcmds+" TotalCMD Length:"+totalcmds.length);
+
+  if(totalcmds.length<2)
+    this.receivedData=totalcmds[0];
+  else{
+    for(let i=0;i<totalcmds.length;i++)
+    {
+      SingleString=totalcmds[i];
+      if(SingleString==='')
+        continue;
+      //console.log("Single String:  "+SingleString);
+
+      var multipleRes=SingleString.split('\r');
+      for(let k=0;k<multipleRes.length;k++)
+      {
+        multiVal=multipleRes[k];
+        if(multiVal==='')
+          continue;
+        console.log("Multi Val:  "+multiVal);
+        //Ans.name=multipleRes.toString().substring(0,4);
+       // Ans.value=multipleRes.toString().substring(5,multipleRes.length)
+       
+        //console.log("Data "+multipleRes+" Count:"+(i++));
+        //var Reply;
+        //Reply=this.parseCommand(multipleRes);
+        //alert("Data Received: "+Reply.value);
+        
+        this.receivedData='';
+        
+      }
+    } 
+  }
+  
+
+}
+parseCommand(hexString)
+{
+  var Rep,ValueArray=[];
+  Rep={};
+  if (hexString === "NO DATA" || hexString === "OK" || hexString === "?" || hexString === "UNABLE TO CONNECT" || hexString === "SEARCHING...") 
+  {
+    //No data or OK is the response, return directly.
+    Rep.value = hexString;
+    return Rep;
+  }
+  //hexString = hexString.replace(/ /g, ''); //Whitespace trimming //Probably not needed anymore?
+  for(let k=0;k<hexString.length;k+=2) 
+    ValueArray.push(hexString?.substring(k,2));
+  if(ValueArray[0]==='41')
+  {
+    Rep.mode=ValueArray[0];
+    Rep.pid=ValueArray[1];
+    for(let j=0;j<obdinfo.PIDS.length;j++)
+    {
+      if(obdinfo.PIDS[j].pid == Rep.pid)
+      {
+        var byte=obdinfo.PIDS[j].bytes;
+        Rep.name=obdinfo.PIDS[j].name;
+        switch(byte){
+          case 1:
+              Rep.value = obdinfo.PIDS[j].convertToUseful(ValueArray[2]);
+              break;
+          case 2:
+              Rep.value = obdinfo.PIDS[j].convertToUseful2(ValueArray[2], ValueArray[3]);
+              break;
+          case 4:
+              Rep.value = obdinfo.PIDS[j].convertToUseful4(ValueArray[2], ValueArray[3], ValueArray[4], ValueArray[5]);
+              break;
+          case 6:
+              Rep.value = obdinfo.PIDS[j].convertToUseful6(ValueArray[2], ValueArray[3], ValueArray[4], ValueArray[5], ValueArray[6], ValueArray[7] );
+              break;
+      }
+      break; //Value is converted, break out the for loop.
+  
+      }
+    }
+  }else if(ValueArray[0]==='43')
+  {
+    Rep.mode=ValueArray[0];
+    for (var ij = 0; ij < obdinfo.PIDS.length; ij++) {
+      if (obdinfo.PIDS[ij].mode == "03") {
+          Rep.name = obdinfo.PIDS[ij].name;
+          Rep.value = obdinfo.PIDS[ij].convertToUseful6(ValueArray[1], ValueArray[2], ValueArray[3], ValueArray[4], ValueArray[5], ValueArray[6]);
+      }
+  }
+  }
+  return Rep;
+     
+}
+InitiateOBD()
+{
+  var self=this;
+  this.btIntervalWriter=setInterval(function(){
+    if(self.queue.length>0 && self.btConnected)
+    {
+      
+      try{
+        var cmd=self.queue.shift();
+        //console.log("Command: "+cmd);
+        self.bluetooth.write(cmd+'\r').then(sk=>{
+          
+        },er=>{
+          self.presentToast("Error Writing Command");
+        })
+
+      }catch(error){
+        self.presentToast("Error Writing Command catch Block");
+        clearInterval(self.btIntervalWriter);
+        this.SavedCarCommand.length=0;
+      }
+    }
+  },this.writeDelay);
+  this.presentToast("OBD Setup Completed");  
+}
+RequstData()
+{
+   var self=this;
+      for (let i = 0; i < self.CarInfo.length; i++) 
+    {
+      if(self.queue.length < 256)
+        self.queue.push(self.CarInfo[i]+1+'\r');
+      else
+        self.presentToast("Queue Overflow");
+    }
+  
+  this.btIntervalWriter=setInterval(function(){
+    if(self.queue.length>0 &&self.btConnected)
+    {
+      try{
+        var cmd=self.queue.shift();
+        self.bluetooth.write(cmd+'\r').then(sk=>{
+          self.presentToast("Wrote Command");
+        },er=>{
+          self.presentToast("Error Writing Command");
+        })
+
+      }catch(error){
+        self.presentToast("Error Writing Command catch Block");
+        clearInterval(self.btIntervalWriter);
+        this.SavedCarCommand.length=0;
+      }
+    }
+  },this.writeDelay);
+
+}
+diconnect()
 {
   this.blue=true;
+  this.btConnected=false;
+  this.queue=['ATZ\r','ATSP0\r','0100\r'];
+  this.SavedCarCommand=[];
+  this.RepeatTimer=0;
+  clearInterval(this.pollerInterval);
+  clearInterval(this.btIntervalWriter)
+  this.isModalOpen=false;
   this.bluetooth.disconnect();
   this.presentToast("Bluetooth Disconnected");
 }
@@ -180,6 +298,7 @@ async  presentToast(msg) {
   
   
 }
+
 }
 interface paired {
   "class": number,
@@ -187,4 +306,11 @@ interface paired {
   "address": string,
   "name": string,
   
+}
+interface obdmetric {
+  "metricSelectedToPoll":boolean,
+  "name":string,
+  "description":string,
+  "value":string,
+  "unit": string
 }
