c5c0f8ecfd1ea314ea3c799df6d3d7cced7e363d
AbdaalZulfi <114151339+AbdaalZulfi@users.noreply.github.com>
2022-11-16 04:32:40 +0300

    Tab1 intial patch

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BluetoothSerial } from '@awesome-cordova-plugins/bluetooth-serial/ngx';
+import { DataSrvService } from '../data-srv.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,10 +10,34 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
   private selectedSegment: string ='Sensor';
-  constructor() {}
+  constructor(public bluetooth:BluetoothSerial, public router:Router,public DataSrv:DataSrvService) {}
 
   segmentChanged(event : any){
   console.log(event.target.value);
   this.selectedSegment=event.target.value;
+  }
+
+  Scan()
+  {
+    this.bluetooth.isEnabled().then(resp=>
+      {
+        this.bluetooth.isConnected().then(rsp=>
+          {
+            this.DataSrv.deviceConnected('03');
+          }
+          ,er=>
+          {
+            console.log("commd dff")
+
+          })
+
+
+      }
+      ,error=>
+      {
+        this.bluetooth.enable();
+        this.router.navigate(['tabs/tab2']);
+      })
+   
   }
 }
