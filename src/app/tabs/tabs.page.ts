5fafaa631455940cbda34034e01f346e3d1821da
AbdaalZulfi <114151339+AbdaalZulfi@users.noreply.github.com>
2022-11-17 16:13:21 +0300

    Committed dwith errors

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
-
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
+    const authObserver = afAuth.authState.subscribe(
+    user => {
+    if (user) {
+    alert("User signed in");
+    this.router.navigate(['/tabs/tab2']);
+    authObserver.unsubscribe();
+    } else {
+    alert("User signed OUT");
+    //this.router.navigate(['/login']); //login page
+    authObserver.unsubscribe();
+    }
+    });
+  }
 }
