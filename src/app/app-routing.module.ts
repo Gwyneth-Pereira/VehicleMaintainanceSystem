801d69c5ed5ed820345a6e9c9d06bfd2d22e56c4
AbdaalZulfi <114151339+AbdaalZulfi@users.noreply.github.com>
2022-11-17 16:07:48 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,6 +28,10 @@ const routes: Routes = [
   {
     path: 'getstarted',
     loadChildren: () => import('./getstarted/getstarted.module').then( m => m.GetstartedPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 
 ];
