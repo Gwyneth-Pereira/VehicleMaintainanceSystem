c296b3cd316b29045adc6a2aa2977e9281a94c40
Gwyneth <gwenap1997@gmail.com>
2022-12-07 13:59:01 +0300

    error codes added up to p100

--- src/app/CodeDesc.ts
+++ src/app/CodeDesc.ts
@@ -1,5 +1,210 @@
 
 
-export class Codes{
+export class codesdesc{
+
+    static codedes=[
+            { code:"P0000" , description:"No trouble code"},  
+            {code:"P0001", description:"Fuel Volume Regulator Control Circuit / Open" },
+            {code:"P0002", description:"Fuel Volume Regulator Control Circuit Range/Performance" },
+            
+            {code:"P0003", description:"Fuel Volume Regulator Control Circuit Low" },
+            {code:"P0004", description:"Fuel Volume Regulator Control Circuit High " },
+            {code:"P0005", description:"Fuel Shutoff Valve Control Circuit / Open " },
+            {code:"P0006", description:"Fuel Shutoff Valve Control Circuit Low " },
+            {code:"P0007", description:"Fuel Shutoff Valve Control Circuit High " },
+            {code:"P0008", description:"Engine Position System Performance - Bank 1 " },
+            {code:"P0009", description:"Engine Position System Performance - Bank 1 " },
+            {code:"P0010", description:"Intake Camshaft Position Actuator Circuit / Open (Bank 1)" },
+
+            {code:"P0011", description:"Intake Camshaft Position Timing - Over-Advanced (Bank 1)" },
+            {code:"P0012", description:"Intake Camshaft Position Timing - Over-Retarded (Bank 1)" },
+            {code:"P0013", description:"Exhaust Camshaft Position Actuator Circuit / Open (Bank 1)" },
+            {code:"P0014", description:"Exhaust Camshaft Position Timing - Over-Advanced (Bank 1)" },
+            {code:"P0015", description:"Exhaust Camshaft Position Timing - Over-Retarded (Bank 1)" },
+            {code:"P0016", description:"Crankshaft Position Camshaft Position Correlation Bank 1 Sensor A" },
+            {code:"P0017", description:"Crankshaft Position Camshaft Position Correlation Bank 1 Sensor B" },
+            {code:"P0018", description:"Crankshaft Position Camshaft Position Correlation Bank 2 Sensor A" },
+            {code:"P0019", description:"Crankshaft Position Camshaft Position Correlation Bank 2 Sensor B" },
+            {code:"P0020", description:" Intake Camshaft Position Actuator Circuit / Open (Bank 2)" },
+            {code:"P0021", description:"Intake Camshaft Position Timing - Over-Advanced (Bank 2)" },
+            {code:"P0022", description:"Intake Camshaft Position Timing - Over-Retarded (Bank 2)" },
+            {code:"P0023", description:"Exhaust Camshaft Position Actuator Circuit / Open (Bank 2)" },
+            {code:"P0024", description:"Camshaft Position B - Timing Over-Advanced or System Performance (Bank 2)" }, 
+            {code:"P0025", description:"Exhaust Camshaft Position Timing - Over-Retarded (Bank 2)" },
+            {code:"P0026", description:"Intake Valve Control Solenoid Circuit Range/Performance (Bank 2)" },
+            {code:"P0027", description:"Exhaust Valve Control Solenoid Circuit Range/Performance (Bank 2)" },
+            {code:"P0028", description:" Intake Valve Control Solenoid Circuit Range/Performance (Bank 2)" },
+            {code:"P0029", description:" Exhaust Valve Control Solenoid Circuit Range/Performance (Bank 2)" },
+            {code:"P0030", description:" Heated Oxygen Sensor (H02S) Heater Control Circuit Bank 1 Sensor 1" },
+            
+            {code:"P0031", description:"Heated Oxygen Sensor (HO2S) Heater Circuit Low Voltage Bank 1 Sensor 1" },
+            {code:"P0032", description:"Heated Oxygen Sensor (HO2S) Heater Circuit High Voltage Bank 1 Sensor 1" },  
+            {code:"P0033", description:"Turbo/Super Charger Bypass Valve Control Circuit / Open " },
+            {code:"P0034", description:"Turbo/Super Charger Bypass Valve Control Circuit Low " },
+            {code:"P0035", description:"  Turbo/Super Charger Bypass Valve Control Circuit High" },
+            {code:"P0036", description:"Heated Oxygen Sensor (HO2S) Heater Control Circuit Bank 1 Sensor 2 " },
+            {code:"P0037", description:"Heated Oxygen Sensor (HO2S) Heater Circuit Low Voltage Bank 1 Sensor 2 " },
+            {code:"P0038", description:" Heated Oxygen Sensor (HO2S) Heater Circuit High Voltage Bank 1 Sensor 2 " },
+            {code:"P0039", description:" Turbo/Super Charger Bypass Valve Control Circuit Range/Performance" },
+            {code:"P0040", description:" Oxygen Sensor Signals Swapped Bank 1 Sensor 1 / Bank 2 Sensor 1 " },
+            {code:"P0041", description:" Oxygen Sensor Signals Swapped Bank 1 Sensor 2 / Bank 2 Sensor 2 " },
+            {code:"P0042", description:" HO2S Heater Control Circuit (Bank 1, Sensor 3) " },
+            {code:"P0043", description:"HO2S Heater Control Circuit Low (Bank 1, Sensor 3) " },
+            {code:"P0044", description:"  HO2S Heater Control Circuit High (Bank 1, Sensor 3)" },
+            {code:"P0046", description:" Turbocharger/Supercharger Boost Control 'A' Circuit Range/Performance " },
+            {code:"P0047", description:"Turbocharger/Supercharger Boost Control 'A' Circuit Low " },
+            {code:"P0048", description:" Turbocharger/Supercharger Boost Control 'A' Circuit High " },
+            {code:"P0049", description:"Turbo/Super Charger Turbine Overspeed " },
+
+            {code:"P0050", description:"  Heated Oxygen Sensor (HO2S) Heater Circuit Bank 2 Sensor 1" },
+            {code:"P0051", description:"Heated Oxygen Sensor (HO2S) Heater Circuit Low Voltage Bank 2 Sensor 1 " },
+            {code:"P051B", description:"   Crankcase Pressure Sensor Circuit Range/Performance" },
+            {code:"P0052", description:"  Heated Oxygen Sensor (HO2S) Heater Circuit High Voltage Bank 2 Sensor 1 " },
+            {code:"P0053", description:"HO2S Heater Resistance Bank 1 Sensor 1 (PCM) " },
+            {code:"P0054", description:" HO2S Heater Resistance Bank 1 Sensor 2 (PCM)" },
+            {code:"P0055", description:" HO2S Heater Resistance Bank 1 Sensor 3 (PCM) " },
+            {code:"P0056", description:"  Heated Oxygen Sensor (HO2S) 2, Bank 2, Heater control - Circuit Malfunction" },
+            {code:"P0057", description:" Heated Oxygen Sensor (HO2S) Heater Circuit Low Voltage Bank 2 Sensor 2" },
+            {code:"P0058", description:" Heated Oxygen Sensor (HO2S) Heater Circuit High Voltage Bank 2 Sensor 2 " }, 
+            {code:"P0059", description:" HO2S Heater Resistance (Bank 2, Sensor 1)" },
+
+            {code:"P0060", description:"HO2S Heater Resistance (Bank 2, Sensor 2) " },
+            {code:"P0061", description:" HO2S Heater Resistance (Bank 2, Sensor 3)" },
+            {code:"P0062", description:"HO2S Heater Control Circuit (Bank 2, Sensor 3) " },
+            {code:"P0063", description:"HO2S Heater Control Circuit Low (Bank 2, Sensor 3) " },
+            {code:"P0064", description:"HO2S Heater Control Circuit High (Bank 2, Sensor 3) " },
+            {code:"P0065", description:"  Air Assisted Injector Control Range/Performance" },
+            {code:"P0066", description:" Air Assisted Injector Control Circuit or Circuit Low " },
+            {code:"P0067", description:"  Air Assisted Injector Control Circuit or Circuit High" },
+            {code:"P0068", description:"MAP / MAF - Throttle Position Correlation " },
+            {code:"P0069", description:"  MAP - Barometric Pressure Correlation " },
+            {code:"P0070", description:" Ambient Air Temperature Sensor Circuit " },
+            {code:"P0071", description:"  Ambient Air Temperature Sensor Range/Performance" },
+            {code:"P0072", description:" Ambient Air Temperature Sensor Circuit Low Input" },
+            {code:"P0073", description:" Ambient Air Temperature Sensor Circuit High Input" },
+            {code:"P0074", description:" Ambient Air Temperature Sensor Circuit Intermittent/Erratic" },
+            {code:"P0075", description:" Intake Valve Control Circuit (Bank 2) " },
+
+            {code:"P0076", description:"Intake Valve Control Circuit Low (Bank 2) " },
+            {code:"P0077", description:" Intake Valve Control Circuit High (Bank 2) " },
+            {code:"P0078", description:"  Exhaust Valve Control Circuit (Bank 2)" },
+            {code:"P0079", description:"Exhaust Valve Control Circuit Low (Bank 2) " },
+            {code:"P0080", description:" Exhaust Valve Control Circuit High (Bank 2) " },
+            {code:"P0081", description:" Intake Valve Control Circuit (Bank 2) " },
+            {code:"P0082", description:" Intake Valve Control Circuit Low (Bank 2)" },
+            {code:"P0083", description:"Intake Valve Control Circuit High (Bank 2) " },
+            {code:"P0084", description:" Exhaust Valve Control Circuit (Bank 2) " },
+            {code:"P0085", description:" Exhaust Valve Control Circuit Low (Bank 2) " },
+            {code:"P0086", description:"  Exhaust Valve Control Circuit High (Bank 2)" },
+            {code:"P0087", description:"Fuel Rail/System Pressure - Too Low " },
+            {code:"P0088", description:"Fuel Rail/System Pressure - Too High " },
+            {code:"P0089", description:"  Fuel Pressure Regulator Performance" },
+            {code:"P0090", description:" Fuel Pressure Regulator Control Circuit " },
+            {code:"P0091", description:"Fuel Pressure Regulator Control Circuit Low " },
+            {code:"P0092", description:" Fuel Pressure Regulator Control Circuit High " },
+            {code:"P0093", description:" Fuel System Leak Detected - Large Leak " },
+            {code:"P0094", description:"Fuel System Leak Detected - Small Leak " },
+            {code:"P0095", description:"Intake Air Temperature Sensor 2 Circuit " },
+            {code:"P0096", description:"  Intake Air Temperature Sensor 2 Circuit Range/Performance" },
+            {code:"P0097", description:" Intake Air Temperature Sensor 2 Circuit Low Input" },
+            {code:"P0098", description: "Intake Air Temperature Sensor 2 Circuit High Input " },
+            {code:"P0099", description:" Intake Air Temperature Sensor 2 Circuit Intermittent/Erratic" },
+            
+            {code:"P00b7", description:"Engine Coolant Flow Low/Performance " },
+            {code:"P0100", description:"Mass or Volume Air flow Circuit Malfunction " },
+            
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+       
+                
+            
+            
+             
+            
+
+ 
+      
+
+
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+
+             
+
+
+ 
+ 
+ 
+ 
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " },
+            {code:"", description:" " }
+
+    ];
     
 }
